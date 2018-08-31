470a3df6d9e0e23e1c9571c72958f0a7803cd53f
Salma Elshaelshahawy <salma71@gmail.com>
2018-08-31 08:04:42 -0400

    refactor code and seperate config routes and services

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+require('./services/passport')
+// const authRoutes = require('./routes/authRoutes')
 
 const app = express()
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken)
-      console.log('refresh token', refreshToken)
-      console.log('profile:    ', profile)
-    }
-  )
-)
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+// authRoutes(app)
+require('./routes/authRoutes')(app)
 
 // means in the production use the provided port
 // otherwise in the development env use port 5000
