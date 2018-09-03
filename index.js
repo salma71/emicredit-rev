4d05adf97dce2d2d7a9261ea81d8f5cd33136ef9
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-03 18:38:59 -0400

    wire up the authentication flow by enable cookie and session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
 
 // reorder the requiring file can cause a DB error when running the server
@@ -11,6 +13,16 @@ require('./services/passport')
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // authRoutes(app)
 require('./routes/authRoutes')(app)
