96c730daf8bd465b451a35989497d6dd4dd11039
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-07 10:42:15 -0400

    add styling to the nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react'
 class Header extends Component {
   render () {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className='nav-wrapper'>
+          <a className='left brand-logo'>
+            Emicredit
+          </a>
+          <ul className='right'>
+            <li>Login with Google</li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
