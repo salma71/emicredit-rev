a2c925ab1c5527ae14674963ffcb2dea8582ea11
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-07 09:46:21 -0400

    finish up the authreducer setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 
 import App from './components/App'
-
+import reducers from './reducers'
 // this store job is to store our application state
-const store = createStore(() => [], {}, applyMiddleware())
+
+const store = createStore(reducers, {}, applyMiddleware())
 
 ReactDom.render(
   // pass the store as a property to the provider component
