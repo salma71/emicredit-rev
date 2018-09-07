3f4ac2124c01bc54079b93cac677323ca38e7673
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-07 13:54:15 -0400

    return the piece of state for auth

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,24 +1,8 @@
 import axios from 'axios'
 import { FETCH_USER } from './types'
 
-// export const fetchUser = () => {
-//   return function (dispatch) {
-//     axios
-//       .get('/api/current_user')
-//       .then(res => dispatch({ type: FETCH_USER, payload: res }))
-//   }
-// }
-
-// // refactoring to ES2017 syntax
-// export const fetchUser = () => async dispatch => {
-//   const res = await axios.get('/api/current_user')
-
-//   dispatch({ type: FETCH_USER, payload: res })
-// }
-
 export const fetchUser = () => async dispatch => {
-  dispatch({
-    type: FETCH_USER,
-    payload: await axios.get('/api/current_user')
-  })
+  const res = await axios.get('/api/current_user')
+
+  dispatch({ type: FETCH_USER, payload: res.data })
 }
