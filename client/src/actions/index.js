5c8f669efd5e99235e5f4d619e33e484683a62c2
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-07 11:47:24 -0400

    more refactor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,9 +9,16 @@ import { FETCH_USER } from './types'
 //   }
 // }
 
-// refactoring to ES2017 syntax
-export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user')
+// // refactoring to ES2017 syntax
+// export const fetchUser = () => async dispatch => {
+//   const res = await axios.get('/api/current_user')
 
-  dispatch({ type: FETCH_USER, payload: res })
+//   dispatch({ type: FETCH_USER, payload: res })
+// }
+
+export const fetchUser = () => async dispatch => {
+  dispatch({
+    type: FETCH_USER,
+    payload: await axios.get('/api/current_user')
+  })
 }
