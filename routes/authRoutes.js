ecd8f3484c5127045feb19fe53db362a9778cb18
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-08 10:31:33 -0400

    redirect after logout to the landing page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,7 +18,8 @@ module.exports = app => {
 
   app.get('/api/logout', (req, res) => {
     req.logout()
-    res.send(req.user)
+    // res.send(req.user)
+    res.redirect('/')
   })
 
   app.get('/api/current_user', (req, res) => {
