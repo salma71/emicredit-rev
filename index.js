d9ff3dca40e93863196a27977b4579ecd80c7aab
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-10 09:48:40 -0400

    install body-parser middleware - submit payment token to the backend.

--- index.js
+++ index.js
@@ -2,32 +2,65 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')
 const keys = require('./config/keys')
-
-// reorder the requiring file can cause a DB error when running the server
-require('./models/user')
+require('./models/User')
 require('./services/passport')
-
-// const authRoutes = require('./routes/authRoutes')
 
 mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, // the session will last for one month
-    keys: [keys.cookieKey] // the key stored in the hidden file
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 )
-
 app.use(passport.initialize())
 app.use(passport.session())
 
-// authRoutes(app)
 require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
 
-// means in the production use the provided port
-// otherwise in the development env use port 5000
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
+
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const cookieSession = require('cookie-session')
+// const passport = require('passport')
+// const bodyParser = require('body-parser')
+// const keys = require('./config/keys')
+
+// // reorder the requiring file can cause a DB error when running the server
+// require('./models/user')
+// require('./services/passport')
+
+// // const authRoutes = require('./routes/authRoutes')
+
+// mongoose.connect(keys.mongoURI)
+
+// const app = express()
+
+// app.use(bodyParser.json())
+
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000, // the session will last for one month
+//     keys: [keys.cookieKey] // the key stored in the hidden file
+//   })
+// )
+
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+// // authRoutes(app)
+// require('./routes/authRoutes')(app)
+// // billingRoutes(app)
+// require('./billingRoutes')(app)
+// // means in the production use the provided port
+// // otherwise in the development env use port 5000
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT)
