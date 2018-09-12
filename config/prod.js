a326842190d280629dec1884d54e20b64fe27498
Salma Elshaelshahawy <salma71@gmail.com>
2018-09-12 09:41:35 -0400

    add mongo username & password

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,8 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
+  mongoUsername: process.env.MONGO_USERNAME,
+  mongoPassword: process.env.MONGO_PASSWORD,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY
